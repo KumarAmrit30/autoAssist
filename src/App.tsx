97d2bb059fefdf50a446c3f0751087292ca9163d
KumarAmrit30 <iamritkumar.30@gmail.com>
2025-09-10 21:06:01 +0530

    Fix React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/autoAssist">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
